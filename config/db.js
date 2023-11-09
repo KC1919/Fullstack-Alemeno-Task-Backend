030c0d9590d537b03f45544c2a2affbc61a57c4c
Kunal Chandra <kunal.chandra1900@gmail.com>
2023-11-10 00:55:24 +0530

    added mongodb atlas db url

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-
+const db_url='mongodb://0.0.0.0:27017/Alemeno-task-db';
 const connectDb = () => {
     try {
-        const conn = mongoose.connect('mongodb://0.0.0.0:27017/Alemeno-task-db');
+        const conn = mongoose.connect(process.env.DB_URI);
 
         if (conn != undefined) {
             console.log("Database connected successfuly!");
