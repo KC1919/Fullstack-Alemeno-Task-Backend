8e89e3828ba445b14e4df7464b82e12f2692fe1c
Kunal Chandra <kunal.chandra1900@gmail.com>
2023-11-10 15:04:55 +0530

    implemented cors

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const courseRouter = require('./routes/course');
 const studentRouter = require('./routes/student');
 const errorHandler = require('./utils/errorFunction.js');
 const PORT = 8000;
+const cors=require('cors');
 
 
 require('dotenv').config({
@@ -14,7 +15,7 @@ require('dotenv').config({
 });
 
 app.use(cookieParser());
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
